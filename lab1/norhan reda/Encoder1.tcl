da1bb622b825466c726ebabbeb824dc822b5dc09
norhanreda <norhanreda12345@gmail.com>
2022-10-08 10:52:20 +0200

    add submission file lab 1 norhan reda

---
+++ lab1/norhan reda/Encoder1.tcl
@@ -0,0 +1,30 @@
+proc runSim {} {
+vlog encoder.v
+vsim encoder
+add wave -position end  sim:/encoder/in
+add wave -position end  sim:/encoder/enable
+add wave -position end  sim:/encoder/out
+
+    
+   for {set i 0} {$i < 256} {incr i} {
+   
+                 
+     # force signals 
+     set binRep [binary format c $i]
+    binary scan $binRep B* binStr
+    set s [string trimleft $binStr 0]
+    
+     force -freeze in  $s 0
+     force -freeze enable  1 0
+     run 
+     
+     force -freeze in  $s 0
+     force -freeze enable  0 0
+     run 
+
+     
+   
+   }
+
+ view wave
+}
---
+++ lab1/norhan reda/encoder.v
@@ -0,0 +1,24 @@
+module encoder(in,enable,out);
+	input [7:0]in;
+        input enable;
+	output reg [2:0]out;
+	
+        always @(in,enable)
+   if(enable==1)
+      begin
+       case(in)
+         8'b00000001:out=0;
+         8'b00000010:out=1;
+         8'b00000100:out=2;
+         8'b00001000:out=3;
+         8'b00010000:out=4;
+         8'b00100000:out=5;
+         8'b01000000:out=6;
+         8'b10000000:out=7;
+         default:out=3'bxxx;
+        endcase
+       end
+    else
+    out=3'bzzz;
+ 
+endmodule
